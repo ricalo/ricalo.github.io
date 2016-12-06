ebcf4b134fea087325005bc0d53d81af635b991a
Ricardo Loo <ricardol@microsoft.com>
2016-12-05 20:44:27 -0800

    Added GitHub metadata configuration

--- bundle_jekyll.sh
+++ bundle_jekyll.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
 bundle install
-jekyll serve --config _config.yml,_config_dev.yml --host 0.0.0.0
+JEKYLL_GITHUB_TOKEN=9cd2ae43355fcdcfe58994cad27c4c188296af7d jekyll serve --config _config.yml,_config_dev.yml --host 0.0.0.0 --force_polling
 
